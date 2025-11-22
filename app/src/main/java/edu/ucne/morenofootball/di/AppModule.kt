cb6bceae19a4468f91b85ec871c0fcbbe2f89a33
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2025-11-22 10:42:20 -0400

    Se modifico el import de la base de datos

--- app/src/main/java/edu/ucne/morenofootball/di/AppModule.kt
+++ app/src/main/java/edu/ucne/morenofootball/di/AppModule.kt
@@ -7,7 +7,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import edu.ucne.morenofootball.data.database.MorenoFootballDb
+import edu.ucne.morenofootball.data.MorenoFootballDb
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
