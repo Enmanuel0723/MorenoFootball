25fb34d82ba53b78477e7b3664489697af748303
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2025-11-28 15:56:55 -0400

    refactor: Remove credit card data module
    Delete all files related to the credit card (`tarjeta`) feature. This includes the `TarjetaApiService` interface, `TarjetaRemoteDataSource` class, and the request/response DTOs (`TarjetaRequest`, `TarjetaResponse`).
    
    Additionally, a new Material Theme project configuration file is added.

--- app/src/main/java/edu/ucne/morenofootball/data/tarjetas/remote/remote/dto/request/TarjetaRequest.kt
+++
@@ -1,9 +0,0 @@
-package edu.ucne.morenofootball.data.tarjetas.remote.remote.dto.request
-
-data class TarjetaRequest(
-    val usuarioId: Int,
-    val bin: String,
-    val cvv: String,
-    val nombreTitular: String,
-    val fechaVencimiento: String,
-)
