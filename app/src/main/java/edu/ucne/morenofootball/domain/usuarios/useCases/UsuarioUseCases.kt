b9ceba7297aa796d7c5ffac505e2cea9645b1b42
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2025-12-02 09:32:05 -0400

    feat: agrega caso de uso para cerrar sesion al respositorio a la clase contenedora de casos de uso

--- app/src/main/java/edu/ucne/morenofootball/domain/usuarios/useCases/UsuarioUseCases.kt
+++ app/src/main/java/edu/ucne/morenofootball/domain/usuarios/useCases/UsuarioUseCases.kt
@@ -5,5 +5,6 @@ data class UsuarioUseCases(
     val loginUseCase: LoginUseCase,
     val modificarCredencialesUseCase: ModificarCredencialesUseCase,
     val validarLoginRegisterUseCase: ValidarLoginRegisterUseCase,
-    val getUsuarioLoggeadoUseCase: GetUsuarioLoggeadoUseCase
+    val getUsuarioLoggeadoUseCase: GetUsuarioLoggeadoUseCase,
+    val logoutUseCase: LogoutUseCase
 )
