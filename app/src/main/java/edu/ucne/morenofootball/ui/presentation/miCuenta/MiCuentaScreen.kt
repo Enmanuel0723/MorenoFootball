31af4585aad673239faa036fe6c738b41998ec49
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2025-12-02 09:27:01 -0400

    feat: Add 'Mi Cuenta' screen with logout functionality
    Create the `MiCuentaScreen` composable, which serves as the user's account page. This screen displays the username, a profile icon, and a menu with options like "Mis Pedidos" and "Métodos de Pago."
    
    It also includes a "Cerrar Sesión" (Logout) button that, when clicked, triggers the logout event in the `MiCuentaViewModel` and navigates the user back to the login screen.

---
+++ app/src/main/java/edu/ucne/morenofootball/ui/presentation/miCuenta/MiCuentaScreen.kt
@@ -0,0 +1,188 @@
+package edu.ucne.morenofootball.ui.presentation.miCuenta
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.twotone.Logout
+import androidx.compose.material.icons.twotone.CreditCard
+import androidx.compose.material.icons.twotone.LocalShipping
+import androidx.compose.material.icons.twotone.Person
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+
+@Composable
+fun MiCuentaScreen(
+    viewModel: MiCuentaViewModel = hiltViewModel(),
+    navigateToLogin: () -> Unit = {},
+) {
+    val state = viewModel.state.collectAsStateWithLifecycle()
+
+    LaunchedEffect(Unit) {
+        viewModel.navigateToLogin.collect {
+            navigateToLogin()
+        }
+    }
+
+    MiCuentaBody(
+        state = state.value,
+        onEvent = viewModel::onEvent
+    )
+}
+
+@Composable
+fun MiCuentaBody(
+    state: MiCuentaUiState,
+    onEvent: (MiCuentaUiEvent) -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(16.dp)
+    ) {
+        // Icono de persona
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 32.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                Icon(
+                    imageVector = Icons.TwoTone.Person,
+                    contentDescription = "Perfil",
+                    modifier = Modifier.size(128.dp),
+                    tint = MaterialTheme.colorScheme.primary
+                )
+                Text(
+                    text = "¡Hola, ${state.username}!",
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
+        }
+
+        // Opciones de menú
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(MaterialTheme.shapes.medium)
+                .background(MaterialTheme.colorScheme.surface)
+        ) {
+            // Mis Pedidos
+            TextButton(
+                onClick = { /* TODO */ },
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.textButtonColors(
+                    contentColor = MaterialTheme.colorScheme.onSurface
+                )
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.TwoTone.LocalShipping,
+                        contentDescription = "Mis Pedidos",
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Text(
+                        text = "Mis Pedidos",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Start
+                    )
+                }
+            }
+
+            HorizontalDivider(
+                modifier = Modifier.fillMaxWidth(),
+                thickness = 1.dp,
+                color = MaterialTheme.colorScheme.outline.copy(alpha = 0.2f)
+            )
+
+            // Metodos de Pago
+            TextButton(
+                onClick = { /* TODO */ },
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.textButtonColors(
+                    contentColor = MaterialTheme.colorScheme.onSurface
+                )
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.TwoTone.CreditCard,
+                        contentDescription = "Métodos de Pago",
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Text(
+                        text = "Métodos de Pago",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Start
+                    )
+                }
+            }
+
+            HorizontalDivider(
+                modifier = Modifier.fillMaxWidth(),
+                thickness = 1.dp,
+                color = MaterialTheme.colorScheme.outline.copy(alpha = 0.2f)
+            )
+
+            // Cerrar sesión
+            TextButton(
+                onClick = { onEvent(MiCuentaUiEvent.Logout) },
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.textButtonColors(
+                    contentColor = MaterialTheme.colorScheme.error
+                )
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.TwoTone.Logout,
+                        contentDescription = "Cerrar Sesión",
+                        tint = MaterialTheme.colorScheme.error
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Text(
+                        text = "Cerrar Sesión",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Start
+                    )
+                }
+            }
+        }
+    }
+}
