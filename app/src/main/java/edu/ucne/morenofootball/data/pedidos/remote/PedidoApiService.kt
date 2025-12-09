1f8b9274a02b2292e1b9f49566ae03aedcae3faf
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2025-12-09 13:34:48 -0400

    feat: Add PedidoApiService for order management
    Define the `PedidoApiService` interface using Retrofit to handle order-related network operations.
    
    This service includes endpoints to:
    - Create a new order via a `POST` request.
    - Fetch lists of orders by user ID for different statuses (`listByUsuarioId`, `listByEntrega`, `listByEnviado`) using `GET` requests.

---
+++ app/src/main/java/edu/ucne/morenofootball/data/pedidos/remote/PedidoApiService.kt
@@ -0,0 +1,23 @@
+package edu.ucne.morenofootball.data.pedidos.remote
+
+import edu.ucne.morenofootball.data.pedidos.remote.dto.request.PedidoRequestDto
+import edu.ucne.morenofootball.data.pedidos.remote.dto.response.PedidoResponseDto
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface PedidoApiService {
+    @GET("api/Pedidos/listByUsuarioId/{usuarioId}")
+    suspend fun listByUsuarioId(@Path("usuarioId") usuarioId: Int): Response<List<PedidoResponseDto>>
+
+    @GET("api/Pedidos/listByEntrega/{usuarioId}")
+    suspend fun listByEntrega(@Path("usuarioId") usuarioId: Int): Response<List<PedidoResponseDto>>
+
+    @GET("api/Pedidos/listByEnviado/{usuarioId}")
+    suspend fun listByEnviado(@Path("usuarioId") usuarioId: Int): Response<List<PedidoResponseDto>>
+
+    @POST("api/Pedidos/create")
+    suspend fun create(@Body request: PedidoRequestDto): Response<Unit>
+}
